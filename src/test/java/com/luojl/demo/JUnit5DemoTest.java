91fba85e1d84920f160a2e51055ce6c4d1bc14d8
Jun Luo <junluo2@illinois.edu>
2020-11-17 22:04:56 -0500

    Make test methods package-private

--- src/test/java/com/luojl/demo/JUnit5DemoTest.java
+++ src/test/java/com/luojl/demo/JUnit5DemoTest.java
@@ -29,64 +29,64 @@ public class JUnit5DemoTest {
     }
 
     @Test
-    public void SimpleTest() {
+    void SimpleTest() {
         assertEquals("Hey there", "Hey there");
     }
 
     @Test
-    public void TestA() {
+    void TestA() {
         System.out.println("[JUnit5] Running Test A");
     }
 
     @Test
-    public void TestB() {
+    void TestB() {
         System.out.println("[JUnit5] Running Test B");
     }
 
     @Test
     @DisplayName("CC")
-    public void TestC() {
+    void TestC() {
         System.out.println("[JUnit5] Running Test C");
     }
 
     @Test
     @Disabled
-    public void disabledTest() {
+    void disabledTest() {
         System.out.println("Should not run");
     }
 
     @Test
     @Order(5)
-    public void testWithOrder5() {
+    void testWithOrder5() {
         System.out.println("test with order 5");
     }
 
     @Test
     @Order(3)
-    public void testWithOrder3() {
+    void testWithOrder3() {
         System.out.println("test with order 3");
     }
 
     @Test
     @Order(10)
-    public void testWithOrder10() {
+    void testWithOrder10() {
         System.out.println("test with order 10");
     }
 
     @Test
-    public void flakytest1() {
+    void flakytest1() {
         assertEquals(0, this.flakyStaticInt);
         this.flakyStaticInt += 1;
     }
 
     @Test
-    public void flakytest2() {
+    void flakytest2() {
         // flakytest1 should run first, otherwise this will fail
         assertEquals(1, this.flakyStaticInt);
     }
 
     @Test
-    public void testIncreaseVal() {
+    void testIncreaseVal() {
         flakyStaticInt2 += 1;
         this.flakyInt += 1;
         System.out.println("increase flakyInt to " + this.flakyInt);
@@ -94,7 +94,7 @@ public class JUnit5DemoTest {
     }
 
     @Test
-    public void testCheckVal() {
+    void testCheckVal() {
         System.out.println("flakyInt: " + this.flakyInt);
         System.out.println("flakyStaticInt2: " + flakyStaticInt2);
     }
